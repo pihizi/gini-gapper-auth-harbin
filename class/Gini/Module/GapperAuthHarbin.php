f9d2486642a7ee54a69128c67e0f2f02bc343998
pihizi <pihizi@msn.com>
2015-03-17 16:55:03 +0800

    update mail info

--- class/Gini/Module/GapperAuthHarbin.php
+++ class/Gini/Module/GapperAuthHarbin.php
@@ -10,6 +10,10 @@ class GapperAuthHarbin
 
     public static function diagnose()
     {
+        $home = \Gini\Config::get('app.home');
+        if (!$home) {
+            return ['请配置app.home'];
+        }
         $secret = \Gini\Config::get('app.harbin_secret');
         if (!$secret) {
             return ['需要提供app.harbin_secret'];
